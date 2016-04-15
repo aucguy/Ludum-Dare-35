ffb29ee91ab21f20c66e0225b8aed8bbf68631d1
aucguy <aucguy@users.noreply.github.com>
2016-04-15 18:57:27 -0500

    added build system

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 var base = base || {};
 base.indexFunc = function indexFunc(state) {
-  base.loadAssets([
-    //libraries
+  var assets = [
+    ['test',                'assets/test.txt',              'text']
+  ];
+  //#mode dev
+  base.loadAssets(assets.concat([
     ['scripts/phaser',      'lib/phaser/dist/phaser.js',    'script'],
     ['scripts/canvg',       'lib/canvg/canvg.js',           'script'],
     ['scripts/rgbcolor',    'lib/canvg/rgbcolor.js',        'script'],
@@ -9,13 +12,14 @@ base.indexFunc = function indexFunc(state) {
     ['scripts/phaserInjector', 'lib/basejs/src/injectors/phaserInjector.js', 'script'],
     ['scripts/stateMachine', 'lib/javascript-state-machine/state-machine.js', 'script'],
 
-    //custom code
     ['scripts/main',        'src/main.js',                  'script'],
     ['scripts/gui',         'src/gui.js',                   'script'],
     ['scripts/util',        'src/util.js',                  'script'],
-
-    //gui
-    ['gui/mainMenu',        'assets/gui/mainMenu.svg',      'text'],
-    ['gui/playGui',         'assets/gui/playGui.svg',       'text'],]
-  ]);
+  ]));
+  //#mode none
+  //#mode rel
+  //base.loadAssets(assets.concat([
+  // ['scripts/app',        'app.min.js',                   'script']
+  //]));
+  //#mode none
 };
